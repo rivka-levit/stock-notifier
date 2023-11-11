76d87548e67bcac76caa21cd710211e4e8ff65cf
rivka-yuliia <juliacreadora@gmail.com>
2023-11-11 23:16:34 +0200

    reformat

--- tests/test_scraping.py
+++ tests/test_scraping.py
@@ -12,14 +12,14 @@ class ScrapingTests(unittest.TestCase):
     """Tests for Chrome driver."""
 
     def setUp(self):
-        self.driver = ChromeClient()
+        self.client = ChromeClient()
 
     def test_chrome_driver_works(self):
         """Test chrome driver access the page and get the response."""
 
-        self.driver.get('https://zse.hr/en/indeks-366/365?isin=HRZB00ICBEX6')
+        self.client.get('https://zse.hr/en/indeks-366/365?isin=HRZB00ICBEX6')
 
-        self.assertIn("CROBEX", self.driver.title)
+        self.assertIn("CROBEX", self.client.title)
 
     def test_crawler_scrapes_trend(self):
         """Test the crawler returns float value of index change."""
